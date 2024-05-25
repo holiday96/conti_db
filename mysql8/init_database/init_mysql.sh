fa69711b1a9dfab785006245c66904a77de5e9a1
XuShiTa <namlun96@gmail.com>
2024-05-25 15:55:31 +0700

    convert line endings CRLF to LF (Windows-style to Unix-style)

--- mysql8/init_database/init_mysql.sh
+++ mysql8/init_database/init_mysql.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 echo " ================== start to initialization of database ..."
 
 export MYSQL_PWD=root
 
+cd /docker-entrypoint-initdb.d/init
 
-mysql -uroot < docker-entrypoint-initdb.d/init/create_database.sql
-mysql -uroot < docker-entrypoint-initdb.d/init/grant_unittest.sql
+mysql -uroot < create_database.sql
+mysql -uroot < grant_unittest.sql
 
 export MYSQL_PWD=unittest
 
 echo " ======  creating utc_java ..."
-mysql -uunittest utc_java < docker-entrypoint-initdb.d/init/utc_java/data-definition.sql
+mysql -uunittest utc_java < utc_java/data-definition.sql
 
 echo " ================== initialization of database finished successfully...."
